9a313c72a21187930896a43545a32f1ab6e4bbaf
Henrique-Git <73835143+Henrique-Git@users.noreply.github.com>
2020-11-27 20:18:28 -0300

    Update resultado_rastreio.php

--- Login_PHP/resultado_rastreio.php
+++ Login_PHP/resultado_rastreio.php
@@ -8,21 +8,22 @@
 <body>
 <?php 
 
-$cod = $_POST['codigo'];
+$cod = $_POST['codigo']; //Recebe o código de rastreio digitado pelo usuário
 
-$post = array('Objetos' => $cod);
-		// iniciar CURL
-$ch = curl_init();
-		// informar URL e outras funções ao CURL
+$post = array('Objetos' => $cod); //Insere o código em um array
+
+$ch = curl_init(); //Inicia CURL
+	
+	//Informa a URL e outras funções ao CURL
 	curl_setopt($ch, CURLOPT_URL, "https://www2.correios.com.br/sistemas/rastreamento/resultado_semcontent.cfm");
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch,CURLOPT_POSTFIELDS, http_build_query($post));
-		// Acessar a URL e retornar a saída
-$output = utf8_encode(curl_exec($ch));
-		// liberar
-	curl_close($ch);
-		// Imprimir a saída
-	echo $output;
+		
+$output = utf8_encode(curl_exec($ch)); //Acessa a URL e retorna a saída
+		
+	curl_close($ch); //Encerra o CURL
+		
+	echo $output; //Imprime a saída
 
 ?>
 
